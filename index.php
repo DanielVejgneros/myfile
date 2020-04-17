48bb33dd80256bea5d25147f15117e635b10740d
DanielVejgneros <62398219+DanielVejgneros@users.noreply.github.com>
2020-04-17 19:51:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,90 @@
 <?php
 
 echo "Ahojdaaaa";
+echo"<br>";
+print "Helooooo";
 
 echo "<p align=\“center\“>Zdavím vás</p>";
-
+//4.vypiš přes "echo" a "print" všechny typy proměnných
 //Boolean
-$mam_malo_penez = TRUE;
-
+echo$mam_malo_penez = TRUE;
+echo"<br>";
 //Integer
-$plat = 10000;
+echo$plat = 10000;
+echo"<br>";
 
 //Float
-$disketa = 3.8;
+print$disketa = 3.8;
+echo"<br>";
 
 //String
-$prohlaseni = "Právě se vracím z karantény! ...";
+print$prohlaseni = "Právě jsem v karanténě! ...";
+echo"<br>";
 
-echo $mam_malo_penez."<BR>";
-echo $plat."<BR>";
-echo $disketa."<BR>";
-echo $prohlaseni."<BR>";
+echo"<br>";
 
-function spocitat($jedna, $dva)
-{
-    echo $jedna . ' + ' . $dva . ' = ' . ($jedna + $dva);
-}
+//5. spojování řetězců
 
-spocitat(10,5);
+print $text = 'Bejt doma je super' . ', ale ne pořád:-(.';
+echo"<br>";
 
+ $a=1;
+ $b=2;
+echo ($a.$b);
+
+echo"<br>";
+echo"<hr>";
+
+//6.img
+echo "<img src='Micina.jpg' alt=\"Kočička\" />";
+
+echo"<hr>";
+
+//7.Zpětné lomítko
+echo"\\";
+
+echo"<br>";
+
+//8.var_dump
+$a = 10;
+$b = 15;
+var_dump($a*$b);
+echo"<br>";
+var_dump($a/$b);
+echo"<br>";
+var_dump($a+$b);
+echo"<br>";
+var_dump($a-$b);
+echo"<br>";
+echo"<br>";
+//přirovnávací operátory
+var_dump($a++);
+echo"<br>";
+var_dump($a--);
+echo"<br>";
+var_dump($a+=$b);
+echo"<br>";
+var_dump($a-=$b);
+echo"<br>";
+var_dump($a*=$b);
+echo"<br>";
+var_dump($a/=$b);
+echo"<br>";
+echo"<br>";
+//porovnávací operátory
+var_dump($a==$b);
+echo"<br>";
+var_dump($a!=$b);
+echo"<br>";
+var_dump($a>$b);
+echo"<br>";
+var_dump($a<$b);
+echo"<br>";
+echo"<br>";
+//logické operátory
+var_dump($a||$b);
+echo"<br>";
+var_dump($a&&$b);
+echo"<br>";
+var_dump(!$a);
 ?>
