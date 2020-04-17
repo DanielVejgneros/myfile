64e166f9f0007e99c222b80c2b7cb5514b5a9258
DanielVejgneros <62398219+DanielVejgneros@users.noreply.github.com>
2020-04-17 18:18:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
 
-phpinfo();
+echo "Ahojdaaaa";
+
+echo "<p align=\“center\“>Zdavím vás</p>";
+
+//Boolean
+$mam_malo_penez = TRUE;
+
+//Integer
+$plat = 10000;
+
+//Float
+$disketa = 3.8;
+
+//String
+$prohlaseni = "Právě se vracím z karantény! ...";
+
+echo $mam_malo_penez."<BR>";
+echo $plat."<BR>";
+echo $disketa."<BR>";
+echo $prohlaseni."<BR>";
+
+function spocitat($jedna, $dva)
+{
+    echo $jedna . ' + ' . $dva . ' = ' . ($jedna + $dva);
+}
+
+spocitat(10,5);
+
+?>
